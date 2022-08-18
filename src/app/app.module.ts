10c7d0c65dd24ff228a12319f89cfe6a28064432
Rolando Oc <admi.jmb@gmail.com>
2022-08-17 20:44:40 -0500

    01 | Components and lazy loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TemplateModule } from './template/template.module';
+import { SharedModule } from './shared/shared.module';
+import { ReactiveModule } from './reactive/reactive.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
+    TemplateModule,
+    ReactiveModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/template/template-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { BasicComponent } from './basic/basic.component';
+import { DynamicComponent } from './dynamic/dynamic.component';
+import { SwitchesComponent } from './switches/switches.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      { path: 'basic', component: BasicComponent },
+      { path: 'dymanic', component: DynamicComponent },
+      { path: 'switches', component: SwitchesComponent },
+      { path: '**', redirectTo: 'basic' },
+    ],
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class TemplateRoutingModule {}
